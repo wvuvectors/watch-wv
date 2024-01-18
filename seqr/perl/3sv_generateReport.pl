40113779f611cc1e84e6f58f8f0019e04e442286
Timothy Driscoll <wvuvectors@gmail.com>
2024-01-18 17:53:16 -0500

    Tweaks to the seqr report format, and report for 1/4 run generated.

--- seqr/perl/3sv_generateReport.pl
+++ seqr/perl/3sv_generateReport.pl
@@ -18,9 +18,11 @@ $usage   .=   "\n";
 while (@ARGV) {
   my $arg = shift;
   if ($arg eq "-h") {
-		die $usage;
+		die "$usage\n";
 	}
 }
+
+my $status = 0;
 
 #
 # Get time stamp for now
@@ -48,7 +50,8 @@ Columns in the seqrDB table:
 	5	sample_collection_end_datetime
 	6 lineage_group
 	7	variant
-	8	variant_proportion
+	8	variant_aliases
+	9	variant_proportion
 =cut
 
 my %sarvar      = ("by_county" => {}, "by_location" => {}, "by_run" => {}, "by_variant" => {}, "by_month" => {});
@@ -72,7 +75,7 @@ open my $fh, "<", "$SARVARDB_MAIN" or die "Unable to open $SARVARDB_MAIN: $!\n";
 while (my $line = <$fh>) {
 	next if "$line" =~ /^sample_id/;
 	chomp $line;
-	my ($sample_id, $sbatch_id, $location, $county, $start, $end, $lineage, $varname, $prop) = split /\t/, "$line", -1;
+	my ($sample_id, $sbatch_id, $location, $county, $start, $end, $lineage, $varname, $aliases, $prop) = split /\t/, "$line", -1;
 	
 	my ($mo, $day, $yr) = (0,0,0);
 	if ($end =~ m/(\d+?)\/(\d+?)\/(\d+?)\s/) {
@@ -117,20 +120,23 @@ while (my $line = <$fh>) {
 		$sarvar{"by_run"}->{"$sbatch_id"} = $sarvar{"by_run"}->{"$sbatch_id"}+1;
 	}
 	
-	if (defined $vtw{"$varname"}) {
-		$vtw{"$varname"}->{"by_county"} = {} unless defined $vtw{"$varname"}->{"by_county"};
-		$vtw{"$varname"}->{"by_county"}->{"$county"} = $datecomp unless defined $vtw{"$varname"}->{"by_county"}->{"$county"};
-		$vtw{"$varname"}->{"by_county"}->{"$county"} = $datecomp if $datecomp > $vtw{"$varname"}->{"by_county"}->{"$county"};
-		$vtw{"$varname"}->{"by_sample"} = {} unless defined $vtw{"$varname"}->{"by_sample"};
-		$vtw{"$varname"}->{"by_sample"}->{"$sample_id"} = 0 unless defined $vtw{"$varname"}->{"by_sample"}->{"$sample_id"};
-		$vtw{"$varname"}->{"by_sample"}->{"$sample_id"} = $vtw{"$varname"}->{"by_sample"}->{"$sample_id"} + $prop;
-	} elsif (defined $vtw{"$lineage"}) {
-		$vtw{"$lineage"}->{"by_county"} = {} unless defined $vtw{"$lineage"}->{"by_county"};
-		$vtw{"$lineage"}->{"by_county"}->{"$county"} = $datecomp unless defined $vtw{"$lineage"}->{"by_county"}->{"$county"};
-		$vtw{"$lineage"}->{"by_county"}->{"$county"} = $datecomp if $datecomp > $vtw{"$lineage"}->{"by_county"}->{"$county"};
-		$vtw{"$lineage"}->{"by_sample"} = {} unless defined $vtw{"$lineage"}->{"by_sample"};
-		$vtw{"$lineage"}->{"by_sample"}->{"$sample_id"} = 0 unless defined $vtw{"$lineage"}->{"by_sample"}->{"$sample_id"};
-		$vtw{"$lineage"}->{"by_sample"}->{"$sample_id"} = $vtw{"$lineage"}->{"by_sample"}->{"$sample_id"} + $prop;
+	my $vocV = isVOC("$varname");
+	my $vocL = isVOC("$lineage");
+	
+	if ("$vocV" ne "") {
+		$vtw{"$vocV"}->{"by_county"} = {} unless defined $vtw{"$vocV"}->{"by_county"};
+		$vtw{"$vocV"}->{"by_county"}->{"$county"} = $datecomp unless defined $vtw{"$vocV"}->{"by_county"}->{"$county"};
+		$vtw{"$vocV"}->{"by_county"}->{"$county"} = $datecomp if $datecomp > $vtw{"$vocV"}->{"by_county"}->{"$county"};
+		$vtw{"$vocV"}->{"by_sample"} = {} unless defined $vtw{"$vocV"}->{"by_sample"};
+		$vtw{"$vocV"}->{"by_sample"}->{"$sample_id"} = 0 unless defined $vtw{"$vocV"}->{"by_sample"}->{"$sample_id"};
+		$vtw{"$vocV"}->{"by_sample"}->{"$sample_id"} = $vtw{"$vocV"}->{"by_sample"}->{"$sample_id"} + $prop;
+	} elsif ("$vocL" ne "") {
+		$vtw{"$vocL"}->{"by_county"} = {} unless defined $vtw{"$vocL"}->{"by_county"};
+		$vtw{"$vocL"}->{"by_county"}->{"$county"} = $datecomp unless defined $vtw{"$vocL"}->{"by_county"}->{"$county"};
+		$vtw{"$vocL"}->{"by_county"}->{"$county"} = $datecomp if $datecomp > $vtw{"$vocL"}->{"by_county"}->{"$county"};
+		$vtw{"$vocL"}->{"by_sample"} = {} unless defined $vtw{"$vocL"}->{"by_sample"};
+		$vtw{"$vocL"}->{"by_sample"}->{"$sample_id"} = 0 unless defined $vtw{"$vocL"}->{"by_sample"}->{"$sample_id"};
+		$vtw{"$vocL"}->{"by_sample"}->{"$sample_id"} = $vtw{"$vocL"}->{"by_sample"}->{"$sample_id"} + $prop;
 	}
 	
 	$sarvar{"by_variant"}->{"$varname"} = 0 unless defined $sarvar{"by_variant"}->{"$varname"};
@@ -160,70 +166,71 @@ print "# NOTE: It indicates the importance of a variant specifically for WV wast
 print "# NOTE: It is informed by CDC variant classification levels (VOI, VOC, etc.).\n#\n";
 
 
-print "\n\n########################################\n";
+print "\n\n############################################################\n";
 print "Overview:";
-print "\n########################################\n\n";
+print "\n############################################################\n\n";
 print "Total samples sequenced     : " . scalar(keys %sample_ids) . ".\n";
 print "Date last sample collected  : " . $sdate{"mo"} . "/" . $sdate{"day"} . "/" . $sdate{"yr"} . ".\n";
 print "Date of last sequencing run : " . $last_run_h{"mo"} . "/" . $last_run_h{"day"} . "/" . $last_run_h{"yr"} . ".\n";
-print "Total variants identified   : " . scalar(keys %{$sarvar{"by_variant"}}) . ".\n";
-print "Total lineages identified   : " . scalar(keys %{$sarvar{"by_lineage"}}) . ".\n";
+print "Total variants identified   : " . scalar(keys %{$sarvar{"by_variant"}}) . ".\n\n";
 
 
-# sort by variant name
-my @vtw_sorted = sort {$vtw{"$a"} cmp $vtw{"$b"}} keys %vtw;
+# sort by variant status
+my @vtw_sorted = sort {$vtw{"$a"}->{"watch_status"} <=> $vtw{"$b"}->{"watch_status"}} keys %vtw;
 
 
-print "\n\n########################################\n";
-print "Distribution of highest concern variants (WaTCH level = 1) across all samples:";
-print "\n########################################\n\n";
+print "\n\n############################################################\n";
+print "WaTCH LEVEL 1 (highest concern) variants";
+print "\n############################################################\n\n";
 foreach my $lineage (@vtw_sorted) {
-	next if $vtw{"$lineage"}->{"watch_status"} > 1;
+	next unless $vtw{"$lineage"}->{"watch_status"} == 1;
 	if (defined $vtw{"$lineage"}->{"by_sample"}) {
-		print "$vtw{$lineage}->{label} variant $lineage found in " . scalar(keys %{$vtw{"$lineage"}->{"by_sample"}}) . " sample";
+		print "$lineage ($vtw{$lineage}->{label}) detected in " . scalar(keys %{$vtw{"$lineage"}->{"by_sample"}}) . " sample";
 		if (scalar(keys %{$vtw{"$lineage"}->{"by_sample"}}) == 1) {
-			print ":\n";
+			print "\n";
 		} else {
-			print "s:\n";
+			print "s\n";
 		}
-		foreach my $sample_id (keys %{$vtw{"$lineage"}->{"by_sample"}}) {
+		print "------------------------------------------------------------\n";
+		foreach my $sample_id (sort keys %{$vtw{"$lineage"}->{"by_sample"}}) {
 			my $sample_ref = $sample_ids{"$sample_id"};
-			print "   $sample_ref->{print_date} (run $sample_ref->{sbatch_id}) -> " . sprintf("%.1f", (100*$vtw{"$lineage"}->{"by_sample"}->{"$sample_id"})) . "% of sample $sample_id from $sample_ref->{location} in $sample_ref->{county} county.\n";
+			print "$sample_ref->{print_date} in $sample_ref->{county} county ($sample_ref->{location}): " . sprintf("%.1f", (100*$vtw{"$lineage"}->{"by_sample"}->{"$sample_id"})) . "% of sample $sample_id, batch $sample_ref->{sbatch_id}\n";
 		}
-		print "\n";
+		print "\n\n";
 	}
 }
 
 
-print "\n\n########################################\n";
-print "Geographic distribution of all variants to watch (WaTCH level <= $warn_level, sorted by most recent detection):";
-print "\n########################################\n\n";
+print "\n\n############################################################\n";
+print "WaTCH LEVEL 2 (high concern) variants";
+print "\n############################################################\n\n";
 foreach my $lineage (@vtw_sorted) {
-	next if $vtw{"$lineage"}->{"watch_status"} > $warn_level;
+	next unless $vtw{"$lineage"}->{"watch_status"} == 2;
 	if (defined $vtw{"$lineage"}->{"by_county"}) {
-		print "$vtw{$lineage}->{label} variant $lineage found in " . scalar(keys %{$vtw{"$lineage"}->{"by_county"}}) . " count";
+		print "$lineage ($vtw{$lineage}->{label}) detected in " . scalar(keys %{$vtw{"$lineage"}->{"by_county"}}) . " count";
 		if (scalar(keys %{$vtw{"$lineage"}->{"by_county"}}) == 1) {
-			print "y:\n";
+			print "y\n";
 		} else {
-			print "ies:\n";
+			print "ies\n";
 		}
-		foreach my $county (sort({$vtw{"$lineage"}->{"by_county"}->{"$b"} <=> $vtw{"$lineage"}->{"by_county"}->{"$a"}} keys %{$vtw{"$lineage"}->{"by_county"}})) {
+		print "------------------------------------------------------------\n";
+		foreach my $county (sort keys %{$vtw{"$lineage"}->{"by_county"}}) {
 			my $datestr = $vtw{"$lineage"}->{"by_county"}->{"$county"};
 			$datestr =~ s/(\d{4})(\d{2})(\d{2})/$2\/$3\/$1/i;
-			print "   $county, most recent detection on $datestr.\n";
+			print "$datestr in $county county\n";
 		}
-		print "\n";
+		print "\n\n";
 	}
 }
 
-print "\n\n########################################\n";
-print "Top 20 most abundant variants across all samples (any  WaTCH level):";
-print "\n########################################\n\n";
+print "\n\n############################################################\n";
+print "Top 20 most abundant variants (any WaTCH level)";
+print "\n############################################################\n\n";
 my @variants_sorted = sort {$sarvar{"by_variant"}->{"$b"} <=> $sarvar{"by_variant"}->{"$a"}} keys %{$sarvar{"by_variant"}};
 my $i = 0;
 foreach my $variant (@variants_sorted) {
 	$i++;
-	print "$sarvar{by_variant}->{$variant} samples contain $variant\n";
+	print "$sarvar{by_variant}->{$variant} samples: $variant\n";
 	last if $i == 20;
 }
 
@@ -231,5 +238,40 @@ print "\n";
 print "\n# EOF.\n";
 
 
-exit;
+exit $status;
+
+
+
+sub isVOC {
+	my $aliases_csv = shift;
+
+	# input contains a comma-sep list of all aliases for this variant (including the "primary" name).
+	# Use @varlist to go through each alias.
+	#
+	my @varlist = split /,/, "$aliases_csv", -1;
+
+	my @voclist = keys %vtw;
+
+	# Check each alias against our list of variants of concern (keys of the %vtw hash).
+	# If found, return 1.
+	# Variants in %vtw may include regex terms so it requires a N^n approach.
+	# Fortunately both arrays are short!
+	#
+	foreach my $v (@varlist) {
+		foreach my $voc (@voclist) {
+			if ("$voc" =~ /\*$/) {
+				my $vocm = "$voc";
+				$vocm =~ s/\*$//;
+				return "$voc" if "$v" =~ /^$vocm/;
+			} else {
+				return "$voc" if "$v" eq "$voc";
+			}
+		}
+	}
+	
+	return "";
+
+}
+
+
 
